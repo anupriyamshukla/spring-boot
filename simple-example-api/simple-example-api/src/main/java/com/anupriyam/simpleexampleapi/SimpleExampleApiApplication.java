d0f67df9adab4ed76cb54dfc3601765e8668aca4
Anupriyam <anupriyamshukla@gmail.com>
2026-07-20 22:14:45 +0530

    Created the Project

---
+++ simple-example-api/simple-example-api/src/main/java/com/anupriyam/simpleexampleapi/SimpleExampleApiApplication.java
@@ -0,0 +1,12 @@
+package com.anupriyam.simpleexampleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExampleApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleExampleApiApplication.class, args);
+    }
+}
---
+++ simple-example-api/simple-example-api/src/test/java/com/anupriyam/simpleexampleapi/SimpleExampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anupriyam.simpleexampleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleExampleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
