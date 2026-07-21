5aae6ae51056ee99bf89e8ab8c64c49af974dd70
Anupriyam <anupriyamshukla@gmail.com>
2026-07-21 22:44:49 +0530

    Added Comment for Hello Controller

--- simple-example-api/simple-example-api/src/main/java/com/anupriyam/simpleexampleapi/HelloController.java
+++ simple-example-api/simple-example-api/src/main/java/com/anupriyam/simpleexampleapi/HelloController.java
@@ -3,6 +3,12 @@ package com.anupriyam.simpleexampleapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Handles GET requests to the "/hello" endpoint.
+ *
+ * @return a welcome message confirming the application is running.
+ */
+
 @RestController
 public class HelloController {
 
