895f5035a99027dccd96554f0c381f24ab60b2be
Anupriyam <anupriyamshukla@gmail.com>
2026-07-23 22:46:57 +0530

    Added comment for add controller

--- simple-example-api/simple-example-api/src/main/java/com/anupriyam/simpleexampleapi/AddController.java
+++ simple-example-api/simple-example-api/src/main/java/com/anupriyam/simpleexampleapi/AddController.java
@@ -7,6 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AddController {
 
+/**
+ * Adds two integer values received through HTTP query parameters.
+ *
+ * Example:
+ * GET /add?a=10&b=20
+ *
+ * Response:
+ * 30
+ *
+ * @param a first operand
+ * @param b second operand
+ * @return sum of the provided operands
+ */
+
     @GetMapping("/add")
     public int add(@RequestParam int a, @RequestParam int b) {
         return a+b;
